6fb8f050d903f9a0c09d84eb9b97b87f15afbebf
BayuTriwibowo18 <144087130+BayuTriwibowo18@users.noreply.github.com>
2023-10-10 14:55:17 +0700

    Add files via upload

---
+++ For07.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class For07 {
+    public static void main(String[] args) {
+        Scanner input07 = new Scanner(System.in);
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hasil kuadrat dari " + 1 + " adalah " + (i * i));
+        }
+    }
+}
